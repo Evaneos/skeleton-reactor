d28d9f7512ef135eca40b79f1be2ad4d56d716dc
Johann Saunier <saunier.johann@gmail.com>
2016-05-24 16:58:20 +0200

    tolerance

--- www/public/index.php
+++ www/public/index.php
@@ -11,4 +11,5 @@ $debug = getenv('SILEX_SKT_DEBUG') !== '0' && $env !== 'prod';
 $kernel = new HttpKernel($env, $debug, (string) Uuid::uuid4());
 $response = $kernel->handle($request = Request::createFromGlobals());
 $response->send();
+
 $kernel->terminate($request, $response);
